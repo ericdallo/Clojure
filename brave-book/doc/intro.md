bf9e787ee7d1536a60c28c2b0f3c0884905803e3
Eric Dallo <ericdallo06@hotmail.com>
2016-02-20 21:58:33 -0200

    Adiciona busca linear em clojure

---
+++ brave-book/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to brave-book
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ brave-book/test/brave_book/core_test.clj
@@ -0,0 +1,7 @@
+(ns brave-book.core-test
+  (:require [clojure.test :refer :all]
+            [brave-book.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
