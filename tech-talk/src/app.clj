c6f84398192d76d28a7d3bddae3f0e1616918081
Eric Dallo <ericdallo06@hotmail.com>
2016-03-20 00:21:08 -0300

    Refatora futures

--- tech-talk/src/app.clj
+++ tech-talk/src/app.clj
@@ -1,5 +1,6 @@
 (ns app 
-  (:gen-class))
+  (:gen-class)
+  (:require [macro :refer :all]))
   
 (defn task-future
   "Print a line before and after a future(another thread)"
@@ -11,14 +12,22 @@
   (println "Post greg"))
 
 (defn default-value
-  "Returns a default value in future Thread timeout"
+  "Returns a default value in future Thread timeout" 
   []
-  (deref (future (Thread/sleep 5000) (123)) 3 (println "Timeout ;(")))
+  (deref (future (Thread/sleep 5000) 123) 2000 "Timeout ;("))
 
 (defn is-finished?
-  []
-  (def over (future (Thread/sleep 2000) (println "End!")))
-  (println (realized? over)))
+  ([]
+    (def over (future (Thread/sleep 2000) (println "End!")))
+    (println (realized? over)))
+  ([wait-for-it]
+    (def over (future (Thread/sleep 2000) (println "End!")))
+    (unless wait-for-it
+      (println (realized? over))
+      (do
+        (Thread/sleep 3000)
+        (println (realized? over))))))
+
 
 (defn -main
   []
